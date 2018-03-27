959b9a2c3ada9c1799b17a96c4c2d71bc13769ed
guaracyalima <guaracyaraujolima@gmail.com>
2018-03-27 07:54:16 -0300

    list view de alunos

--- app/src/main/java/me/rangar/guabirabadev/agenda/ListaAlunosActivity.java
+++ app/src/main/java/me/rangar/guabirabadev/agenda/ListaAlunosActivity.java
@@ -2,6 +2,8 @@ package me.rangar.guabirabadev.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class ListaAlunosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); //recupera o comportamento da classe extendida
         setContentView(R.layout.activity_lista_alunos);
+
+        String[] alunos = {"Minguazinha", "Sopinha", "Teste123"};
+
+        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+
+        listaAlunos.setAdapter(adapter);
     }
 }
